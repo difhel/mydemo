1e138519b4416cf348b8840818d291d525e15b9e
Mark Fomin <mark@difhel.dev>
2025-03-17 18:03:42 +0300

    init

---
+++ src/@types/mytonwallet.d.ts
@@ -0,0 +1,20 @@
+export declare global {
+    interface Window {
+        mytonwalletClient?: {
+            v: string;
+            $internal: {
+                stylesInjected: boolean;
+                virtualKeyboardHeightChangeReady: boolean;
+            }
+        }
+        'dapp-mainbutton'?: {
+            show: () => void;
+            hide: () => void;
+            setParams: (params: { text: string; textColor: string; color: string; disabledColor: string; isVisible: boolean; isActive: boolean; }) => void;
+            onClick: (callback: () => void) => void;
+            offClick: (callback: () => void) => void;
+            showProgress: () => void;
+            hideProgress: () => void;
+        }
+    }
+}
---
+++ src/App.tsx
@@ -0,0 +1,108 @@
+import { useState, useRef, useEffect } from 'react'
+import './App.css'
+import useForceUpdate from './util/useForceUpdate';
+
+function App() {
+  const [mainButtonText, setMainButtonText] = useState('Click me');
+  const isMainButtonShownRef = useRef(false);
+  const isMainButtonActiveRef = useRef(true);
+  const isMainButtonProgressShownRef = useRef(false);
+  const [mainButtonTextColor, setMainButtonTextColor] = useState('#000');
+  const [mainButtonColor, setMainButtonColor] = useState('#fff');
+  const [mainButtonDisabledColor, setMainButtonDisabledColor] = useState('#00000050');
+  const forceUpdate = useForceUpdate();
+
+  const updateMainButton = () => {
+    window['dapp-mainbutton']?.setParams({
+      text: mainButtonText,
+      textColor: mainButtonTextColor,
+      color: mainButtonColor,
+      disabledColor: mainButtonDisabledColor,
+      isVisible: isMainButtonShownRef.current,
+      isActive: isMainButtonActiveRef.current
+    });
+  };
+
+  useEffect(() => {
+    window['dapp-mainbutton']?.onClick(() => {
+      alert('clicked');
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>MyTonWallet Demo DApp</h1>
+      <ul>
+        <li>
+          Is MyTonWallet environment? {window.mytonwalletClient ? <span className="ok">true</span> : <span className="error">false</span>}
+        </li>
+        <li>
+          MyTonWallet version: <b>{window.mytonwalletClient?.v}</b>
+        </li>
+        <li>
+          MyTonWallet styles injected: {window.mytonwalletClient?.$internal.stylesInjected ? <span className="ok">true</span> : <span className="error">false</span>}
+        </li>
+        <li>
+          window.visualViewport patched for virtual keyboard: {window.mytonwalletClient?.$internal.virtualKeyboardHeightChangeReady ? <span className="ok">true</span> : <span className="error">false</span>}
+        </li>
+      </ul>
+
+      <div>
+        <h2>Main Button</h2>
+        <div className="item">
+          <label>Text</label>
+          <input type="text" value={mainButtonText} onChange={(e) => setMainButtonText(e.target.value)} />
+          <button onClick={updateMainButton}>Update</button>
+        </div>
+        <div className="item">
+          <label>Text color</label>
+          <input type="text" value={mainButtonTextColor} onChange={(e) => setMainButtonTextColor(e.target.value)} />
+          <button onClick={updateMainButton}>Update</button>
+        </div>
+        <div className="item">
+          <label>Background color</label>
+          <input type="text" value={mainButtonColor} onChange={(e) => setMainButtonColor(e.target.value)} />
+          <button onClick={updateMainButton}>Update</button>
+        </div>
+        <div className="item">
+          <label>Disabled background color</label>
+          <input type="text" value={mainButtonDisabledColor} onChange={(e) => setMainButtonDisabledColor(e.target.value)} />
+          <button onClick={updateMainButton}>Update</button>
+        </div>
+        <div className="item final">
+          <button onClick={() => {
+            isMainButtonShownRef.current = !isMainButtonShownRef.current;
+            forceUpdate();
+            updateMainButton();
+          }}>{isMainButtonShownRef.current ? 'Hide' : 'Show'}</button>
+          <button onClick={() => {
+            isMainButtonActiveRef.current = !isMainButtonActiveRef.current;
+            forceUpdate();
+            updateMainButton();
+          }}>{isMainButtonActiveRef.current ? 'Disable' : 'Enable'}</button>
+          <button onClick={() => {
+            isMainButtonProgressShownRef.current = !isMainButtonProgressShownRef.current;
+            forceUpdate();
+            if (isMainButtonProgressShownRef.current) {
+              window['dapp-mainbutton']?.showProgress();
+            } else {
+              window['dapp-mainbutton']?.hideProgress();
+            }
+          }}>{isMainButtonProgressShownRef.current ? 'Hide progress' : 'Show progress'}</button>
+        </div>
+      </div>
+      <div>
+        <h2>Auth</h2>
+        <div className="item final">
+          <button onClick={() => {
+            (window as any).invokeNativeFunc('client:dapp-verify-password', [], (result: any) => {
+              console.log(result);
+            });
+          }}>Start auth</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
